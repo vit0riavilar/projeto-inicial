6a1a18782a40405bc8ce1e665199df0da8fbaaf4
vit0riavilar <vitoria.ovilar@gmail.com>
2024-03-14 09:48:52 -0300

    mensagem contém nome

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
-    alert("Você clicou no botão!!!!"+ event);
+    alert("Você clicou no botão!!!!"+ nome.value);
 }
